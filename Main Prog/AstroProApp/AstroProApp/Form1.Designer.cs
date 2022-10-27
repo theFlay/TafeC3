a7c2aaa8ee4a9f142163f4f3bb0a5f2e8a44b136
theFlay <116256646+theFlay@users.noreply.github.com>
2022-10-27 20:58:42 +0800

    GUI and Sort
    Made First GUI
    Added bubble sort and array initialization

--- Main Prog/AstroProApp/AstroProApp/Form1.Designer.cs
+++ Main Prog/AstroProApp/AstroProApp/Form1.Designer.cs
@@ -28,13 +28,119 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.labelTxtBx = new System.Windows.Forms.Label();
+            this.labelData = new System.Windows.Forms.Label();
+            this.textBoxMain = new System.Windows.Forms.TextBox();
+            this.buttonGen = new System.Windows.Forms.Button();
+            this.buttonSort = new System.Windows.Forms.Button();
+            this.buttonSearch = new System.Windows.Forms.Button();
+            this.listBoxMain = new System.Windows.Forms.ListBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.SuspendLayout();
+            // 
+            // labelTxtBx
+            // 
+            this.labelTxtBx.AutoSize = true;
+            this.labelTxtBx.Location = new System.Drawing.Point(9, 12);
+            this.labelTxtBx.Name = "labelTxtBx";
+            this.labelTxtBx.Size = new System.Drawing.Size(56, 13);
+            this.labelTxtBx.TabIndex = 0;
+            this.labelTxtBx.Text = "Enter Text";
+            // 
+            // labelData
+            // 
+            this.labelData.AutoSize = true;
+            this.labelData.Location = new System.Drawing.Point(9, 81);
+            this.labelData.Name = "labelData";
+            this.labelData.Size = new System.Drawing.Size(33, 13);
+            this.labelData.TabIndex = 1;
+            this.labelData.Text = "Data:";
+            // 
+            // textBoxMain
+            // 
+            this.textBoxMain.Location = new System.Drawing.Point(12, 28);
+            this.textBoxMain.Name = "textBoxMain";
+            this.textBoxMain.Size = new System.Drawing.Size(237, 20);
+            this.textBoxMain.TabIndex = 2;
+            this.textBoxMain.TextChanged += new System.EventHandler(this.textBoxMain_TextChanged);
+            // 
+            // buttonGen
+            // 
+            this.buttonGen.Location = new System.Drawing.Point(12, 54);
+            this.buttonGen.Name = "buttonGen";
+            this.buttonGen.Size = new System.Drawing.Size(75, 23);
+            this.buttonGen.TabIndex = 3;
+            this.buttonGen.Text = "Generate";
+            this.buttonGen.UseVisualStyleBackColor = true;
+            this.buttonGen.Click += new System.EventHandler(this.buttonGen_Click);
+            // 
+            // buttonSort
+            // 
+            this.buttonSort.Location = new System.Drawing.Point(93, 54);
+            this.buttonSort.Name = "buttonSort";
+            this.buttonSort.Size = new System.Drawing.Size(75, 23);
+            this.buttonSort.TabIndex = 4;
+            this.buttonSort.Text = "Sort";
+            this.buttonSort.UseVisualStyleBackColor = true;
+            this.buttonSort.Click += new System.EventHandler(this.buttonSort_Click);
+            // 
+            // buttonSearch
+            // 
+            this.buttonSearch.Location = new System.Drawing.Point(174, 54);
+            this.buttonSearch.Name = "buttonSearch";
+            this.buttonSearch.Size = new System.Drawing.Size(75, 23);
+            this.buttonSearch.TabIndex = 5;
+            this.buttonSearch.Text = "Search";
+            this.buttonSearch.UseVisualStyleBackColor = true;
+            this.buttonSearch.Click += new System.EventHandler(this.buttonSearch_Click);
+            // 
+            // listBoxMain
+            // 
+            this.listBoxMain.FormattingEnabled = true;
+            this.listBoxMain.Location = new System.Drawing.Point(12, 97);
+            this.listBoxMain.Name = "listBoxMain";
+            this.listBoxMain.Size = new System.Drawing.Size(237, 290);
+            this.listBoxMain.TabIndex = 6;
+            this.listBoxMain.SelectedIndexChanged += new System.EventHandler(this.listBoxMain_SelectedIndexChanged);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Location = new System.Drawing.Point(0, 404);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(261, 22);
+            this.statusStrip1.TabIndex = 7;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(261, 426);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.listBoxMain);
+            this.Controls.Add(this.buttonSearch);
+            this.Controls.Add(this.buttonSort);
+            this.Controls.Add(this.buttonGen);
+            this.Controls.Add(this.textBoxMain);
+            this.Controls.Add(this.labelData);
+            this.Controls.Add(this.labelTxtBx);
+            this.Name = "Form1";
+            this.Text = "Astro Pro";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label labelTxtBx;
+        private System.Windows.Forms.Label labelData;
+        private System.Windows.Forms.TextBox textBoxMain;
+        private System.Windows.Forms.Button buttonGen;
+        private System.Windows.Forms.Button buttonSort;
+        private System.Windows.Forms.Button buttonSearch;
+        private System.Windows.Forms.ListBox listBoxMain;
+        private System.Windows.Forms.StatusStrip statusStrip1;
     }
 }
 
--- Main Prog/AstroProApp/AstroProApp/Form1.cs
+++ Main Prog/AstroProApp/AstroProApp/Form1.cs
@@ -8,13 +8,69 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+// Peter Halligan, BigBooleans, Sprint One
+// Date: 27/10/22
+// Version: 1.0
+// Astro Pro
+// Generates an array of randomised data to simulate neutrino measurements in low atmosthphere
+// Inputs, Processes, Outputs
+
+
 namespace AstroProApp
 {
     public partial class Form1 : Form
     {
+        //set empty array with length of 24
+        private int[] dataArray = new int[24];
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void buttonGen_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonSort_Click(object sender, EventArgs e)
+        {
+            #region
+            //initial variables
+            int tempBubble = 0;
+
+            //sets index for first number
+            for ( int left = 0; left < dataArray.Length; left++ )
+            {
+                //sets index for second number
+                for (int right = 0; right < dataArray.Length; right++ )
+                {
+                    //compares the two
+                    if (dataArray[right] < dataArray[right +1])
+                    {
+                        //if if 1<2 move 1 over 2 via temp var
+                        tempBubble = dataArray[right + 1];
+                        dataArray[right + 1] = dataArray[right];
+                        dataArray[right] = tempBubble;
+                    }
+                }
+            }
+            #endregion
+        }
+
+        private void buttonSearch_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBoxMain_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBoxMain_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
